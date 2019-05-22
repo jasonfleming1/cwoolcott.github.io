e8878f2a920239afb8a452bd9ecde29b422a5ce8
Chris_remote <chris.woolcott@quotacy.com>
2019-05-21 21:06:44 -0500

    add 18.2

---
+++ bootcamp_activities/activities18.2/jobtrack_readme.md
@@ -0,0 +1,15 @@
+**STEP 1:** Sign in to **JobTrack** at https://jobtrack.trilogyed.com using BCS credentials
+
+**STEP 2:** Install the Chrome extension from https://chrome.google.com/webstore/detail/jobtrack-for-chrome/ojhnaheochknmclpabkpialdgkkcboni)
+
+**Step 3:** Find three jobs you think you’ll be interested in:
+
+Visit Dice.com, Indeed.com, Angel.co, or go to the career website of a company you’re interested in
+
+If Chrome extension installed, open it on the job posting and enter the details
+
+If no Chrome extension, copy the URL, switch to JobTrack and enter the URL, Company Name and Job Title
+
+When ready to apply to jobs, return to JobTrack and use these jobs as a starting point
+
+*Career Director will discuss these jobs on coaching call to identify what your ideal job is and how to attain it*
---
+++ bootcamp_activities/activities18.2/solved/06-Scrape-Starter_chris/server.js
@@ -0,0 +1,44 @@
+var cheerio = require("cheerio");
+
+var axios = require("axios");
+var Filter = require('bad-words'); 
+//I mean, I am using the Onion
+var customFilter = new Filter({ placeHolder: 'X'});
+
+// First, tell the console what server.js is doing
+console.log("\n***********************************\n" +
+            "Grabbing every thread name and link\n" +
+            "from The Onion's local board:" +
+            "\n***********************************\n");
+
+// Making a request for reddit's "webdev" board. The page's HTML is passed as the callback's third argument
+ axios.get("https://local.theonion.com/").then(function(response) {
+
+  // Load the HTML into cheerio and save it to a variable
+  // '$' becomes a shorthand for cheerio's selector commands, much like jQuery's '$'
+  var $ = cheerio.load(response.data);
+
+  // An empty array to save the data that we'll scrape
+  var results = [];
+  
+  // With cheerio, find each p-tag with the "title" class
+  // (i: iterator. element: the current element)
+  $("section.content-meta__headline__wrapper").each(function(i, element) {
+
+    // Save the text of the element in a "title" variable
+    var title = customFilter.clean($(element).text());
+
+    // In the currently selected element, look at its child elements (i.e., its a-tags),
+    // then save the values for any "href" attributes that the child elements may have
+    var link = $(element).children().attr("href");
+
+    // Save these results in an object that we'll push into the results array we defined earlier
+    results.push({
+      title: title,
+      link: link
+    });
+  });
+
+  // Log the results once you've looped through each of the elements found with cheerio
+  console.log(results);
+});
